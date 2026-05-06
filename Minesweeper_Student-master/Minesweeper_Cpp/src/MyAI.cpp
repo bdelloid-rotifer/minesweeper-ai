acbbe24f5188e26d6ca4df0f2e3786b570096535
Alayna Lay <220069931+bdelloid-rotifer@users.noreply.github.com>
2026-05-06 15:57:54 -0700

    Added comments and notes to start with Minimal AI [NOTES.txt, MyAI.cpp]

--- Minesweeper_Student-master/Minesweeper_Cpp/src/MyAI.cpp
+++ Minesweeper_Student-master/Minesweeper_Cpp/src/MyAI.cpp
@@ -36,6 +36,55 @@ Agent::Action MyAI::getAction( int number )
     // YOUR CODE BEGINS
     // ======================================================================
 
+    /*
+    C = columns
+    R = rows
+    M = mines
+    Effective label = tile number - number of marked mine neighbors
+    successful game: UNCOVER #C*#R-#M times then LEAVE
+    getAction(...)
+        { 
+        are we done?: #CoveredTiles = #Mines -> LEAVE otherwise need to figure out UNCOVER X,Y
+        > two types of covered tiles: (1) unmarked, (2) marked
+        ...
+        use simple rule of thumb logic -> UNCOVER X,Y
+        > if effective label is 0, eg the tile says 3 and we already marked 3 neighboring tiles as mines, all the other tiles are safe to UNCOVER
+        OR 
+        > if effective label is the same as the # unknown neighboring tiles, they can all be marked as mines
+        ...
+        if this did not give tile to uncover, use better logic -> UNCOVER X,Y
+    `   > look at neighboring numbered tiles to see if we can figure out if an uncovered tile is safe,
+        > eg. tile A: x + y = 1. tile B: x + y + z = 1. By process of elimination z must = 0. so we can uncover z (it's safe).
+        ...
+        if this did not give tile to uncover, go to even more sophisticated logic -> UNCOVER X,Y
+        > AAAAAAAAAAAAAAAAAAAAAAAAAA
+        ...
+        finally, if still no tile (this will happen since some partial boards are undecidable), need to guess 
+        - exact probability computation may be intractable, use approximation -> UNCOVER X,Y
+        > p(mine) = #solutions_with_mine/#solutions. Pick tile with smallest p(mine)
+        }
+    */
+
+    /* TIMEOUT LOGIC (from slides)
+    
+    double total_time_elapsed = 0.0
+    ...
+    Action getAction(...)
+    {
+        double remaining_time = ...
+        if (remaining_time < some_small_number_eg_3)
+            make_random_move
+        else {
+            tS = time_stamp_now
+            // do your normal stuff
+            tE = time_stamp_now
+            dt = tE - tS // time used for this getAction() call
+            total_time_elapsed += dt
+        }
+    }
+    
+    */
+
 
     return {LEAVE,-1,-1};
     // ======================================================================
